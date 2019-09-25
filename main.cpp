885618cfac80500ae48154bd8818c597bc7c8e8e
Ruben9922 <Ruben9922@users.noreply.github.com>
2019-09-25 19:42:28 +0100

    Create shapes

--- main.cpp
+++ main.cpp
@@ -1,11 +1,59 @@
 #include <ncurses.h>
 #include "mathfu/vector.h"
+#include "mathfu/glsl_mappings.h"
+#include <list>
+#include <vector>
 
 int main() {
     initscr();
     cbreak();
     noecho();
     keypad(stdscr, TRUE);
+
+    std::vector<std::list<mathfu::vec2i>> shapes = {
+            {
+                    {0, 0},
+                    {0, 1},
+                    {0, 2},
+                    {0, 3},
+            },
+            {
+                    {1, 0},
+                    {0, 0},
+                    {0, 1},
+                    {0, 2},
+            },
+            {
+                    {0, 0},
+                    {0, 1},
+                    {0, 2},
+                    {1, 2},
+            },
+            {
+                    {0, 0},
+                    {0, 1},
+                    {1, 0},
+                    {1, 1},
+            },
+            {
+                    {0, 0},
+                    {0, 1},
+                    {1, 1},
+                    {1, 2},
+            },
+            {
+                    {0, 0},
+                    {0, 1},
+                    {0, 2},
+                    {1, 1},
+            },
+            {
+                    {0, 1},
+                    {0, 2},
+                    {1, 0},
+                    {1, 1},
+            },
+    };
 
     endwin();
     return 0;
