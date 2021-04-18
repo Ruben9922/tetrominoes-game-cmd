360f927493cbe673ca4a473376463675d3ba08d4
Ruben9922 <Ruben9922@users.noreply.github.com>
2021-04-18 18:01:39 +0100

    Use NumPy and my new utilities package

--- tetrominoes-game-cmd.py
+++ tetrominoes-game-cmd.py
@@ -1,95 +1,74 @@
 import curses
 from random import randrange
-import copy
+import numpy as np
+import game_utilities as gu
 
 
-class Shape:
-    def __init__(self, cells):
-        self.cells = cells
+def create_shape():
+    all_points = [
+        np.array([
+            [0, 0],
+            [0, 1],
+            [0, 2],
+            [0, 3],
+        ]),
+        np.array([
+            [1, 0],
+            [0, 0],
+            [0, 1],
+            [0, 2],
+        ]),
+        np.array([
+            [0, 0],
+            [0, 1],
+            [0, 2],
+            [1, 2],
+        ]),
+        np.array([
+            [0, 0],
+            [0, 1],
+            [1, 0],
+            [1, 1],
+        ]),
+        np.array([
+            [0, 0],
+            [0, 1],
+            [1, 1],
+            [1, 2],
+        ]),
+        np.array([
+            [0, 0],
+            [0, 1],
+            [0, 2],
+            [1, 1],
+        ]),
+        np.array([
+            [0, 1],
+            [0, 2],
+            [1, 0],
+            [1, 1],
+        ]),
+    ]
+    points = all_points[randrange(len(all_points))]
+    shape = gu.Shape(points)
+    return shape
 
-    def size(self):
-        xs = list(map(lambda c: c[1], self.cells))
-        ys = list(map(lambda c: c[0], self.cells))
-        return ((max(ys) - min(ys) + 1) if ys else 0), ((max(xs) - min(xs) + 1) if xs else 0)
 
+def create_shape_instance(stdscr):
+    # Pick random shape
+    shape = create_shape()
 
-class ShapeInstance:
-    def __init__(self, stdscr, position, shape):
-        self.stdscr = stdscr
-        self.position = position
-        self.shape = shape
-
-    def draw(self):
-        for cell in self.shape.cells:
-            self.stdscr.addch(self.position[0] + cell[0], self.position[1] + cell[1], "x")
-
-    def get_cells_world(self):
-        return map(lambda c: (self.position[0] + c[0], self.position[1] + c[1]), self.shape.cells)
-
-    def is_within_window(self, translation=(0, 0)):
-        return all(map(lambda c: 0 <= c[0] + translation[0] < self.stdscr.getmaxyx()[0]
-                                 and 0 <= c[1] + translation[1] < self.stdscr.getmaxyx()[1], self.get_cells_world()))
+    # Initially position so that shape is in top centre
+    return gu.GameObject(stdscr, shape, char="x",
+                         position=gu.align(stdscr, shape.size, gu.HorizontalAlignment.CENTER, gu.VerticalAlignment.TOP),
+                         velocity=gu.DOWN)
 
 
 class Game:
-    all_shapes = [
-        Shape([
-            (0, 0),
-            (0, 1),
-            (0, 2),
-            (0, 3)
-        ]),
-        Shape([
-            (1, 0),
-            (0, 0),
-            (0, 1),
-            (0, 2)
-        ]),
-        Shape([
-            (0, 0),
-            (0, 1),
-            (0, 2),
-            (1, 2)
-        ]),
-        Shape([
-            (0, 0),
-            (0, 1),
-            (1, 0),
-            (1, 1)
-        ]),
-        Shape([
-            (0, 0),
-            (0, 1),
-            (1, 1),
-            (1, 2)
-        ]),
-        Shape([
-            (0, 0),
-            (0, 1),
-            (0, 2),
-            (1, 1)
-        ]),
-        Shape([
-            (0, 1),
-            (0, 2),
-            (1, 0),
-            (1, 1)
-        ])
-    ]
-
     def __init__(self, stdscr):
         self.stdscr = stdscr
         self.shape_instances = []
-        self.moving_shape_instance = self.generate_shape_instance()
-
-    def generate_shape_instance(self):
-        # Pick random shape
-        shape = Game.all_shapes[randrange(len(Game.all_shapes))]
-
-        # Initially position so that shape is in top centre
-        position = (0, (self.stdscr.getmaxyx()[1] - shape.size()[1]) // 2)
-
-        return ShapeInstance(self.stdscr, position, shape)
+        self.moving_shape_instance = create_shape_instance(stdscr)
 
     def play(self):
         self.stdscr.timeout(500)
@@ -106,18 +85,22 @@ class Game:
 
     def update(self, key):
         if key == curses.KEY_LEFT:
-            velocity = (0, -1)
+            translation = gu.LEFT
+            self.moving_shape_instance.velocity = np.array([0, 0])
         elif key == curses.KEY_RIGHT:
-            velocity = (0, 1)
+            translation = gu.RIGHT
+            self.moving_shape_instance.velocity = np.array([0, 0])
         else:
-            velocity = (1, 0)
+            translation = gu.ZERO
+            self.moving_shape_instance.velocity = np.array([1, 0])
 
-        if self.moving_shape_instance.is_within_window(velocity) and not self.check_for_collision(velocity):
-            self.moving_shape_instance.position = (self.moving_shape_instance.position[0] + velocity[0],
-                                                   self.moving_shape_instance.position[1] + velocity[1])
+        self.moving_shape_instance.position += translation
+        if self.moving_shape_instance.is_within_window and not self.check_for_collision():
+            self.moving_shape_instance.update()
         else:
+            self.moving_shape_instance.velocity = np.array([0, 0])
             self.shape_instances.append(self.moving_shape_instance)
-            self.moving_shape_instance = self.generate_shape_instance()
+            self.moving_shape_instance = create_shape_instance(self.stdscr)
 
     def draw(self):
         self.stdscr.clear()
@@ -127,18 +110,22 @@ class Game:
 
         self.moving_shape_instance.draw()
 
-    def check_for_collision(self, translation=(0, 0)):
+    def check_for_collision(self):
         # Translate the moving shape instance's cells by the given translation
-        shape_instance_cells_translated = map(lambda c: (c[0] + translation[0], c[1] + translation[1]),
-                                              self.moving_shape_instance.get_cells_world())
+        # shape_instance_cells_translated = map(lambda c: (c[0] + translation[0], c[1] + translation[1]),
+        #                                       self.moving_shape_instance.get_cells_world())
         # For each shape instance, check whether its cells are disjoint from the moving shape instances's cells
         # (i.e. whether their intersection is empty - none of the cells overlap)
         # If not, there has been a collision between this shape instance and the moving shape instance
         # Obviously want to return True if the moving shape instance has collided with *any* shape instance (hence using
         # `any` function)
         # In other words, check whether the moving shape instance's cells overlap with any shape instance's cells
-        return any(map(lambda si: not set(shape_instance_cells_translated).isdisjoint(si.get_cells_world()),
-                       self.shape_instances))
+        # return any(map(lambda si: not set(shape_instance_cells_translated).isdisjoint(si.get_cells_world()),
+        #                self.shape_instances))
+
+        # return gu.intersect2d(self.moving_shape_instance.cells_world, np.vstack([x.cells_world for x in self.shape_instances]))
+
+        return any(self.moving_shape_instance.collides_with(x) for x in self.shape_instances)
 
 
 def main(stdscr):
